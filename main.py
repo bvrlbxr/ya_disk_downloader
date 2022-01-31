324c00d24bbdd2c673429214f6a2a8c974f5b973
bvrlbxr <alexeymleontiev@gmail.com>
2022-01-31 18:05:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import yadisk
+from config import token as my_tkn
+from config import source_dir
+from config import dest_dir
 
-y = yadisk.YaDisk(token="your token here")
+
+y = yadisk.YaDisk(token=my_tkn)
 count = 0
 count1 = 0
-for i in list(y.listdir("your dir in yandex disk")):
+for i in list(y.listdir(source_dir)):
 	count += 1
 print(f"всего файлов: {count}")
 
 
-for i in list(y.listdir("your dir in yandex disk")):
+for i in list(y.listdir(source_dir)):
 	file_name = i['name']
-	y.download(f'your dir in yandex disk/{file_name}', f'your dir for downloading{file_name}')
+	y.download(f'{source_dir}{file_name}', f'{dest_dir}{file_name}')
 	count1 += 1
 	print(f'успешно скачан {file_name}')
 	print(f'всего скачано: {count1}')
